6e689d2ec35a0be148fe8ecc35c2dd3d89fdb589
JoaoSalvador <jpvsalvador05@gmail.com>
2022-09-13 19:03:45 +0100

    Dynamic main section data

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,77 @@ import React from 'react'
 import styled from 'styled-components'
 import Section from './Section'
 
+const data = [
+    {
+        id: 0,
+        title: "Model S",
+        description: "Order Online for Touchless Delivery",
+        backgroundImg: "model-s.jpg",
+        leftBtnText: "Custom Order",
+        rightBtnText: "Existing Inventory",
+    },
+    {
+        id: 1,
+        title: "Model Y",
+        description: "Order Online for Touchless Delivery",
+        backgroundImg: "model-y.jpg",
+        leftBtnText: "Custom Order",
+        rightBtnText: "Existing Inventory",
+    },
+    {
+        id: 2,
+        title: "Model 3",
+        description: "Order Online for Touchless Delivery",
+        backgroundImg: "model-3.jpg",
+        leftBtnText: "Custom Order",
+        rightBtnText: "Existing Inventory",
+    },
+    {
+        id: 3,
+        title: "Model X",
+        description: "Order Online for Touchless Delivery",
+        backgroundImg: "model-x.jpg",
+        leftBtnText: "Custom Order",
+        rightBtnText: "Existing Inventory",
+    },
+    {
+        id: 4,
+        title: "Lowest Cost Solar Panels in America",
+        description: "Money-back guarantee",
+        backgroundImg: "solar-panel.jpg",
+        leftBtnText: "Order Now",
+        rightBtnText: "Learn More",
+    },
+    {
+        id: 5,
+        title: "Solar for New Roofs",
+        description: "Solar Roof Costs Less Than a New Roof Plus Solar Panels",
+        backgroundImg: "solar-roof.jpg",
+        leftBtnText: "Order Now",
+        rightBtnText: "Learn More",
+    },
+    {
+        id: 6,
+        title: "Accessories",
+        backgroundImg: "accessories.jpg",
+        leftBtnText: "Shop Now",
+    },
+]
+
 function Home() {
     return (
         <Container>
-            <Section />
+            {data.map(data => {
+                return <Section
+                    title={data.title}
+                    description={data.description}
+                    backgroundImg={data.backgroundImg}
+                    leftBtnText={data.leftBtnText}
+                    rightBtnText={data.rightBtnText}
+                    key={data.id}
+                />
+            })}
+
         </Container>
     )
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
 
-function Section() {
+function Section({ title, description, backgroundImg, leftBtnText, rightBtnText }) {
     return (
-        <Container>
+        <Container backgroundImg={backgroundImg}>
             <ItemText>
-                <h1>Model 3</h1>
-                <p>Order Online for Touchless Delivery</p>
+                <h1>{title}</h1>
+                <p>{description}</p>
             </ItemText>
 
             <Buttons>
                 <ButtonGroup>
                     <LeftButton>
-                        Custom Order
+                        {leftBtnText}
                     </LeftButton>
-                    <RightButton>
-                        Existing Inventory
-                    </RightButton>
+                    {rightBtnText &&
+                        <RightButton>
+                            {rightBtnText}
+                        </RightButton>
+                    }
                 </ButtonGroup>
                 <DownArrow src="/images/down-arrow.svg" />
             </Buttons>
@@ -32,7 +34,7 @@ const Container = styled.div`
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
-    background-image: url('/images/model-3.jpg');
+    background-image: ${props => `url("/images/${props.backgroundImg}")`};
     display: flex;
     flex-direction: column;
     justify-content: space-between;
@@ -50,7 +52,12 @@ const Buttons = styled.div`
 
 const ButtonGroup = styled.div`
     display: flex;
+    justify-content: center;
     margin-bottom: 30px;
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
 `
 
 const LeftButton = styled.div`
